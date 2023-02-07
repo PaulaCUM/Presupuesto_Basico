ca0fad33d937e83915870d2a64b2323a1b1945e6
Paula Useche <paula.cum2@gmail.com>
2023-02-06 23:16:18 -0500

    Edit_Borrar_Reiniciar

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
@@ -13,8 +14,9 @@ export class ListarGastoComponent implements OnDestroy, OnInit {
   presupuesto : number;
   restante : number;
   listGastos: any[] = [];
+  valorBorrado: any[] = [];
 
-  constructor (private _presupuestoService : PresupuestoService) {
+  constructor (private _presupuestoService : PresupuestoService, private router: Router) {
     
     this.presupuesto = 0;
     this.restante = 0;
@@ -23,6 +25,7 @@ export class ListarGastoComponent implements OnDestroy, OnInit {
       // console.log(data);
       // Cogemos el restante y le restamos la cantidad que va llegando
       this.restante = this.restante - data.cantidad;
+      this.valorBorrado.push(data.cantidad);
       // Agregamos al arreglo el nuevo dato obtenido por el observable
       this.listGastos.push(data);
     })
@@ -49,4 +52,45 @@ export class ListarGastoComponent implements OnDestroy, OnInit {
     }
 
   }
+
+  borrarGasto(_id: number) {
+    if(confirm("Está seguro de querer borrar este gasto?")){      
+      this.restante = this.restante + this.valorBorrado[_id];
+      this._presupuestoService.restante = this.restante;
+      this.listGastos.splice(_id,1)
+      this.valorBorrado.splice(_id,1)
+    }
+  }
+
+  editarGasto (_id: number) {
+    let nombre = prompt("Introduce el nuevo nombre del gasto", this.listGastos[_id].nombre);
+    let cantidad = prompt("Introduce el nuevo valor del gasto", this.listGastos[_id].cantidad);
+    if (nombre && cantidad) {
+      this.restante = this.restante + this.valorBorrado[_id] - parseInt(cantidad);
+      this._presupuestoService.restante = this.restante;
+      this.listGastos[_id] = { nombre: nombre, cantidad: parseInt(cantidad) };
+      this.valorBorrado[_id] = parseInt(cantidad);
+}
+  }
+
+  borrarTodo() {
+    if(confirm("Está seguro de querer borrar todos los gastos?")){      
+      this._presupuestoService.restante = this.presupuesto;
+      this.restante = this.presupuesto;
+      this.listGastos = [];
+      this.valorBorrado = [];
+    }
+  }
+
+  Reiniciar () {
+    if(confirm("Está seguro de querer reiniciar el presupuesto?")){      
+      this.router.navigate(['/ingresarPresupuesto'])
+      this.presupuesto = 0;
+      this.restante = 0;
+      this._presupuestoService.presupuesto = 0;
+      this._presupuestoService.restante = 0;
+    }
+  }
+
+
 }
